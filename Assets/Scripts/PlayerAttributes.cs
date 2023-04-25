feafb07244f24356bf631855c91715ac2592e819
Yuri Katayama <yurikbcarvalho@gmail.com>
2023-04-25 02:45:00 -0700

    Player Attributes
    Player Attributes class

---
+++ Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour {
+
+    [Header ("Basic Stats")]
+    public int hp;
+    public int movSpeed;
+    public int fireRate;
+    public int walkDir;
+    public int shootDir;
+
+    [Header ("Player Objects")]
+    public Transform player;
+    public Transform gunPoint;
+
+    [Header ("State Booleans")]
+    public bool isAlive;
+    public bool isWalking;
+    public bool isShooting;
+    public bool canChangeState;
+
+    [Header ("State Machine")]
+    public StateMachine State;
+    public enum StateMachine {
+        IDLE_UP,
+        IDLE_LEFT,
+        IDLE_RIGHT,
+        IDLE_DOWN,
+
+        WALKING_UP,
+        WALKING_LEFT,
+        WALKING_RIGHT,
+        WALKING_DOWN,
+
+        SHOOTING_UP,
+        SHOOTING_LEFT,
+        SHOOTING_RIGHT,
+        SHOOTING_DOWN,
+
+        SHOOTING_UP_WALKING_DOWN,
+        SHOOTING_UP_WALKING_LEFTRIGHT,
+
+        SHOOTING_DOWN_WALKING_UP,
+        SHOOTING_DOWN_WALKING_LEFTRIGHT,
+
+        SHOOTING_LEFT_WALKING_RIGHT,
+        SHOOTING_LEFT_WALKING_UPDOWN,
+
+        SHOOTING_RIGHT_WALKING_LEFT,
+        SHOOTING_RIGHT_WALKING_UPDOWN,
+
+        #region  Just in case?
+        // WALKING_UP_SHOOTING_LEFT,
+        // WALKING_UP_SHOOTING_RIGHT,
+        // WALKING_UP_SHOOTING_DOWN,
+
+        // WALKING_LEFT_SHOOTING_UP,
+        // WALKING_LEFT_SHOOTING_RIGHT,
+        // WALKING_LEFT_SHOOTING_DOWN,
+
+        // WALKING_RIGHT_SHOOTING_UP,
+        // WALKING_RIGHT_SHOOTING_LEFT,
+        // WALKING_RIGHT_SHOOTING_DOWN,
+
+        // WALKING_DOWN_SHOOTING_UP,
+        // WALKING_DOWN_SHOOTING_LEFT,
+        // WALKING_DOWN_SHOOTING_RIGHT
+        #endregion
+    }
+
+    public void StateManager (StateMachine newState) => State = canChangeState ? newState : State;
+
+}
